6a3bf902c1b52e178cc872c9ec76cca50d159bb4
arseum <manbrozer@gmail.com>
2023-06-04 16:00:37 +0200

    merge attauqe_special & dev

--- src/main/java/fr/montreuil/iut/kalos_pokemon/modele/Togepi.java
+++
@@ -1,8 +0,0 @@
-package fr.montreuil.iut.kalos_pokemon.modele;
-
-public class Togepi extends Ennemi {
-    public Togepi(int x, int y, Game g) {
-        super(5, 100, "normal", x, y, 50, "togepi", g);
-    }
-
-}
