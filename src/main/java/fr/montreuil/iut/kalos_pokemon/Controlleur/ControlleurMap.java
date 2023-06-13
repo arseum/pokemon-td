ff61602db652e37091b2b30fd60aa3732d96502b
Zen H. A-K <test@test.fr>
2023-06-13 21:36:05 +0200

    resolution merge

--- src/main/java/fr/montreuil/iut/kalos_pokemon/Controlleur/ControlleurMap.java
+++ src/main/java/fr/montreuil/iut/kalos_pokemon/Controlleur/ControlleurMap.java
@@ -13,6 +13,7 @@ import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
