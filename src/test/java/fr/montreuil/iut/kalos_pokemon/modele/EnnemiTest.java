0baf205b63542bfb1d28f2424a619a584b5b37c8
Zen H. A-K <test@test.fr>
2023-06-19 00:35:56 +0200

    Test 2/4 Ennemi

---
+++ src/test/java/fr/montreuil/iut/kalos_pokemon/modele/EnnemiTest.java
@@ -0,0 +1,97 @@
+package fr.montreuil.iut.kalos_pokemon.modele;
+
+import fr.montreuil.iut.kalos_pokemon.Parametres;
+import fr.montreuil.iut.kalos_pokemon.modele.Ennemis.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class EnnemiTest {
+
+    //resetVItesse
+    //reduitVitesseMax
+    //reduitVitesse
+    @Test
+    public final void diminueHPTest(){
+        Game g= new Game("savane");
+        Ludicolo ludicolo = new Ludicolo(0*32, 3*32, g);
+        g.ajouteEnnemi(ludicolo);
+
+        assertEquals(ludicolo.getHp(), 250);
+        assertTrue(g.getListEnnemi().contains(ludicolo));
+        assertEquals(850, g.getPokedollar());
+
+        ludicolo.diminueHP(249);
+        assertEquals(ludicolo.getHp(), 1);
+        assertTrue(g.getListEnnemi().contains(ludicolo));
+        assertEquals(850, g.getPokedollar());
+
+        ludicolo.diminueHP(2);
+        assertEquals(ludicolo.getHp(), -1);
+        assertFalse(g.getListEnnemi().contains(ludicolo));
+        assertEquals(895, g.getPokedollar());
+    }
+    private void parcoursToutCheminSensPositif(Ennemi e, int positionInitiale, int vitesseEnnemi, int longueurChemin, boolean selonX){
+        int position = positionInitiale;
+        for(int i = 0; i < longueurChemin - 1; i++){
+            while((position + vitesseEnnemi) < (i+1) * Parametres.tailleTuile){
+                position = position + vitesseEnnemi;
+                e.bouge();
+                if(selonX) assertEquals(e.getX(), position);
+                else assertEquals(e.getY(), position);
+            }
+            position = Parametres.tailleTuile * (i+1);
+            e.bouge();
+            if(selonX) assertEquals(e.getX(), position);
+            else assertEquals(e.getY(), position);
+        }
+        while ((position + vitesseEnnemi) < longueurChemin * Parametres.tailleTuile){
+            position = position + vitesseEnnemi;
+            e.bouge();
+            if(selonX) assertEquals(e.getX(), position);
+            else assertEquals(e.getY(), position);
+        }
+    }
+
+    private void parcoursToutCheminSensNegatif(Ennemi e, int positionInitiale, int vitesseEnnemi, int longueurChemin, boolean selonX){
+        int position = positionInitiale;
+        for(int i = longueurChemin; i > 1; i--){
+            while((position - vitesseEnnemi) > (i-1) * Parametres.tailleTuile){
+                position = position - vitesseEnnemi;
+                e.bouge();
+                if(selonX) assertEquals(e.getX(), position);
+                else assertEquals(e.getY(), position);
+            }
+            position = Parametres.tailleTuile * (i-1);
+            e.bouge();
+            if(selonX) assertEquals(e.getX(), position);
+            else assertEquals(e.getY(), position);
+        }
+    }
+
+    @Test
+    public final void bougeTest(){
+        Game gameDplVersBas = new Game("deplacementTestVersBas");
+        Game gameDplVersHaut = new Game("deplacementTestVersHaut");
+        Game gameDplVersGauche = new Game("deplacementTestVersGauche");
+        Game gameDplVersDroite = new Game("deplacementTestVersDroite");
+
+        Togepi togepi = new Togepi(4*32, 0*32, gameDplVersBas);
+        parcoursToutCheminSensPositif(togepi, 0, 3, 9, false);
+        assertTrue(togepi.estArrive);
+
+        Tiplouf tiplouf = new Tiplouf(4*32, 9*32, gameDplVersHaut);
+        parcoursToutCheminSensNegatif(tiplouf,9*32,4,9,false);
+        assertTrue(tiplouf.estArrive);
+
+        Camerupt camerupt = new Camerupt(0*32, 4*32, gameDplVersDroite);
+        parcoursToutCheminSensPositif(camerupt, 0, 1, 9, true);
+        assertTrue(camerupt.estArrive);
+
+        Ludicolo lidiculo = new Ludicolo(9*32, 4*32, gameDplVersGauche);
+        parcoursToutCheminSensNegatif(lidiculo,9*32,2,9,true);
+        assertTrue(lidiculo.estArrive);
+
+    }
+}
